6db6e98f1d10bf35aa340d390b261b6a76af5f83
Paul Jones <paulvjones@gmail.com>
2017-02-28 10:59:20 -0700

    encoding string required for fs.readFileSync

--- examples/toJSON.js
+++ examples/toJSON.js
@@ -2,7 +2,7 @@ var dna = require('../');
 var fs = require('fs');
 var path = require('path');
 
-var txt = fs.readFileSync(path.join(__dirname, 'dna.txt'));
+var txt = fs.readFileSync(path.join(__dirname, 'dna.txt'), 'utf-8');
 
 dna.parse(txt, function(err, snps){
   fs.writeFileSync(path.join(__dirname, 'dna.json'), JSON.stringify(snps));
