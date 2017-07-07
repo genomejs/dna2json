e6db50f99e26ea2abbef3699ed731677407cd27c
Zhaofeng Li <hello@zhaofeng.li>
2017-07-07 01:24:08 -0700

    Make dna2json browser-friendly
    This commit removes `require-dir` which depends on `fs`

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-var requireDir = require('require-dir');
-var path = require('path');
-var providers = requireDir(path.join(__dirname, './lib/parsers'));
-
 module.exports = {
   parse: require('./lib/parse'),
-  providers: providers
+  providers: require('./lib/parsers'),
 };
